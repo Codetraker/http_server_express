544de3ba84ebac5940ad55e036d7644165db5011
Codetraker <swarajbibhuprasad@gmail.com>
2024-08-30 20:21:39 +0530

    http server using express

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+
+const app = express();
+
+const users = [{
+    name : "john",
+    kidney : [{
+        healthy : false
+    }]
+}];
+
+app.use(express.json());
+
+app.get("/", function(req,res){
+    const jhonKidney = users[0].kidney;
+    const numberOfKidney = jhonKidney.length;
+    const healthyKidney = jhonKidney.filter(function(item){
+        return item.healthy === true;
+    })
+    const numberOfHealthyKidney = healthyKidney.length;
+    const numberOfUnhealthyKidney = numberOfKidney - numberOfHealthyKidney;
+
+    res.json({
+        numberOfKidney,
+        numberOfHealthyKidney,
+        numberOfUnhealthyKidney
+    })
+})
+
+app.post("/", function(req,res){
+    const isHealthy = req.body.isHealthy;
+    users[0].kidney.push({
+        healthy : isHealthy
+    })
+    res.json({
+        msg : "Posting DONE"
+    })
+})
+
+app.put("/", function(req,res){
+    for(let i=0;i<users[0].kidney.length;i++){
+        users[0].kidney[i].healthy = true;
+    }
+    res.json({msg : "Puting DONE"});
+})
+
+app.delete("/", function(req,res){
+    //if atleast one unhealthy kidney is there do this, else return 411
+    if(isAnyUnhealthyKidney()){
+        const newKidney = [];
+        for(let i=0;i<users[0].kidney.length;i++){
+            if(users[0].kidney[i].healthy){
+                newKidney.push({
+                    healthy : true
+                })
+            }
+        }
+        users[0].kidney = newKidney;
+        res.json({msg : "Deletion DONE"});
+    }else{
+        res.status(411).json({
+            msg : "You have no bad kidney"
+        });
+    }
+})
+
+function isAnyUnhealthyKidney(){
+    let atleastOneUnhealthyKidney = false;
+    for(let i=0;i<users[0].kidney.length;i++){
+        if(!users[0].kidney[i].healthy){
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney;
+}
+
+app.listen(3000);
